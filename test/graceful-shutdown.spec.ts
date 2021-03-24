bc334b91ac6c9a7ec5054c3939baf15f3e6416be
Francesco Rivola <francesco.rivola@gmail.com>
2021-03-24 23:27:32 +0100

    some fixes

--- test/graceful-shutdown.spec.ts
+++ test/graceful-shutdown.spec.ts
@@ -2,17 +2,20 @@ import gracefulShutdown from '../src/graceful-shutdown';
 import { App } from '../src/app';
 
 describe('graceful-shutdown', () => {
-
     afterEach(() => {
         jest.clearAllMocks();
         jest.resetAllMocks();
     });
 
     it('should call app.close and process exit 0', async () => {
-        const app = {
+        const app = ({
             close: jest.fn(() => Promise.resolve())
-        } as unknown as App;
-        const exitStub = jest.spyOn(process, 'exit').mockImplementation((() => { return; }) as unknown as (code?: number) => never);
+        } as unknown) as App;
+        const exitStub = jest
+            .spyOn(process, 'exit')
+            .mockImplementation(((() => {
+                return;
+            }) as unknown) as (code?: number) => never);
 
         await gracefulShutdown(app)();
 
@@ -22,10 +25,14 @@ describe('graceful-shutdown', () => {
     });
 
     it('should call app.close and process exit 1 when app.close throws an error', async () => {
-        const app = {
+        const app = ({
             close: jest.fn(() => Promise.reject('Oops, an error'))
-        } as unknown as App;
-        const exitStub = jest.spyOn(process, 'exit').mockImplementation((() => { return; }) as unknown as (code?: number) => never);
+        } as unknown) as App;
+        const exitStub = jest
+            .spyOn(process, 'exit')
+            .mockImplementation(((() => {
+                return;
+            }) as unknown) as (code?: number) => never);
 
         await gracefulShutdown(app)();
 
