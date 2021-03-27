7040242cb5fe4ea5c252eef83ab55b9c6e837812
Francesco Rivola <francesco.rivola@gmail.com>
2021-03-27 18:41:24 +0100

    Revert "added swagger parser"
    This reverts commit b3fdbcf712579d506b4cfbd5d41b7d3557ce5e09.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import { FastifyInstance } from 'fastify';
 import { Config } from './config';
 import { buildLogger } from './logger';
 import { buildServer } from './server';
-import SwaggerParser from '@apidevtools/swagger-parser';
 
 export type App = {
     close(): Promise<void>;
@@ -10,13 +9,8 @@ export type App = {
 };
 
 export async function buildApp(config: Config): Promise<App> {
-    const { specFile, log } = config;
-    if (!specFile) {
-        throw new Error('OpenApi specification file is required');
-    }
-    const openApi = await SwaggerParser.validate(specFile);
-    const logger = buildLogger(log);
-    const server = buildServer(logger, openApi);
+    const logger = buildLogger(config.log);
+    const server = buildServer(logger);
     return {
         async close(): Promise<void> {
             await server.close();
